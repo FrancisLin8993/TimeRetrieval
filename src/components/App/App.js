0b8e6415bfd9572b86b59b015abc18e79a007e48
Francis <flin0002@student.monash.edu>
2020-05-01 14:22:21 +1000

    set up header main and footer folders and files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
