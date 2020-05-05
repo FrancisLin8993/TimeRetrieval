996e6150e7929bd1382a878dfbab61600539ffc7
FrancisLin8993 <FrancisLin8993@users.noreply.github.com>
2020-05-05 05:00:48 +0000

    deploy: 3b35ea6df716399056471c0efc19501c763f8a4a

---
+++ precache-manifest.f3f8f4356dc8a0847a9a5667489c019f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592d66971fb4471f73b533193f010312",
+    "url": "/TimeRetrieval/index.html"
+  },
+  {
+    "revision": "e97023adaedd6d6433ea",
+    "url": "/TimeRetrieval/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "e9ff31de806513c7f30b",
+    "url": "/TimeRetrieval/static/js/2.5a9fa15e.chunk.js"
+  },
+  {
+    "revision": "01f28827afd20553593a551e1ee6c97b",
+    "url": "/TimeRetrieval/static/js/2.5a9fa15e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e97023adaedd6d6433ea",
+    "url": "/TimeRetrieval/static/js/main.ac1dcef3.chunk.js"
+  },
+  {
+    "revision": "1ce1b224f4ac6e53cdac",
+    "url": "/TimeRetrieval/static/js/runtime-main.36d03058.js"
+  }
+]);
